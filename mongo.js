65df0b4a398d7023cf7978a3a4b8c5053e7bdd93
Ondairos <ondairos@gmail.com>
2023-01-11 13:13:06 +0200

    added command lien arguments to storing persons

--- mongo.js
+++ mongo.js
@@ -3,43 +3,68 @@ require('dotenv').config();
 
 const url = process.env.MONGO_URI;
 
-// if (process.argv.length < 3) {
-//   console.log('Please provide the password as an argument: node mongo.js <password>')
-//   process.exit(1)
-// }
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
 
-// const password = process.argv[2]
+const password = process.argv[2] //third argument password
+const name = process.argv[3] //fourth argument name
+const number = process.argv[4] //fifth argument number
 
 
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
+const personSchema = new mongoose.Schema({
+    password: String,
+    name: String,
+    number: Number,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
 mongoose
     .connect(url)
     .then((result) => {
         console.log('connected')
 
-        const note = new Note({
-            content: 'HTML is Easy',
-            date: new Date(),
-            important: true,
+        const person = new Person({
+            password: password,
+            name: name,
+            number: number,
         })
-
-        const note2 = new Note({
-            content: "lorem  ipsum ok",
-            date: new Date(),
-            important: false,
-        })
-
-        return Promise.all([note.save(), note2.save()])
+        return person.save()
     })
-    .then(() => {
-        console.log('note saved!')
-        return mongoose.connection.close()
+    .then((res) => {
+        console.log(`${res.name} saved to the database!`)
+        return Person.find({});
+
+    }).then(result => {
+        if (result.length > 0) {
+            console.log('Phonebook: ');
+            result.forEach(person => {
+                console.log(person.name)
+            });
+        } else {
+            console.log('No persons found');
+        }
+        mongoose.connection.close()
     })
     .catch((err) => console.log(err))
+
+
+// mongoose
+//     .connect(url)
+//     .then((result) => {
+//         console.log('connected')
+//         return Note.find({ important: true });
+//     })
+//     .then(result => {
+//         if (result.length > 0) {
+//             result.forEach(note => {
+//                 console.log(note)
+//             });
+//         } else {
+//             console.log('No notes found');
+//         }
+//         mongoose.connection.close()
+//     })
+//     .catch((err) => console.log(err))
