4cadb71a88417c157680aea69de5e9cbc9940a55
Ondairos <ondairos@gmail.com>
2023-01-13 14:23:04 +0200

    small change for eslint

--- index.js
+++ index.js
@@ -94,7 +94,9 @@ app.get('/api/persons/:id', (request, response) => {
 app.get('/info', (request, response) => {
     Person.countDocuments().then(count => {
         let numberOfEntries = count
+        // eslint-disable-next-line no-undef
         date = new Date()
+        // eslint-disable-next-line no-undef
         response.end(`<p>Phonebook has info for ${numberOfEntries} people!</p><p>The date is: ${date}.</p>`)
     }).catch(err => console.log(err))
 
